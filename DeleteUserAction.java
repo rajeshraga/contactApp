fa85d731af94d2044f030be4b5eaf44b9d3792ab
rajeshraga <rajeshraga@gmail.com>
2016-08-23 16:41:48 -0500

    Update DeleteUserAction.java

--- DeleteUserAction.java
+++ DeleteUserAction.java
@@ -1,7 +1,8 @@
 package com.contactApp.action;
 
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -11,39 +12,47 @@ import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import com.contactApp.beans.provideUserService;
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.util.ValueStack;
 
 public class DeleteUserAction {
 	
 	private String uid;
-	private List<String> delUserList;	
-	
-	public List<String> getDelUserList() {
-		return delUserList;
+	private Map<Integer,String> userchkList;
+	private List<UserInfoData> userdata;
+	public List<UserInfoData> getUserdata() {
+		return userdata;
 	}
 
-	public void setDelUserList(List<String> delUserList) {
-		this.delUserList = delUserList;
+	public void setUserdata(List<UserInfoData> userdata) {
+		this.userdata = userdata;
 	}
 
-	public String getUid() {
-		return uid;
-	}
-
-	public void setUid(String uid) {
-		this.uid = uid;
+	public Map<Integer, String> getUserchkList() {
+		return userchkList;
 	}
 
 	public String execute() {
-		System.out.println("here we have delete request");		
+		System.out.println("here we have delete request");
 		
-		WebApplicationContext context = WebApplicationContextUtils.getRequiredWebApplicationContext(ServletActionContext.getServletContext());
-		
+		WebApplicationContext context = WebApplicationContextUtils.getRequiredWebApplicationContext(ServletActionContext.getServletContext());		
 		provideUserService UsrService = (provideUserService)context.getBean("provideUserService");
-		int i = Integer.parseInt(uid);
-		UsrService.deleteUsersFromList(ServletActionContext.getRequest(), i);
+		
+		HttpServletRequest request = ServletActionContext.getRequest();		
+		String val = request.getParameter("allVals");
+		List<String> delList = Arrays.asList(val.split(","));
+		
+		UsrService.deleteUsersFromList(ServletActionContext.getRequest(), delList);
 		
 	
 		return "success";
 	}
+
+	public void setUserchkList(Map<Integer, String> userchkList) {
+		this.userchkList = userchkList;
+	}
+
+	
+	
 
 }
